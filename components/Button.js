a6fc6e4c1a622b1715a7331ac2a08b8cf3f90040
Stephan2a <stephan.gueorguieff@upikajob.com>
2023-06-30 16:31:15 +0200

    prop_access for component Button

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,3 @@
-//export default function Button({ title, onClick, style }) {
-//  const baseStyle = {
-//    backgroundColor: "grey",
-//    borderRadius: "5px",
-//  };
-//  return createElement(
-//    "button",
-//    {
-//      onClick: onClick,
-//      style: { ...baseStyle, ...style },
-//    },
-//    [title]
-//  );
-//}
-
 export default function Button({ title, onClick, style }) {
   const baseStyle = {
     backgroundColor: "grey",
@@ -27,5 +12,37 @@ export default function Button({ title, onClick, style }) {
       click: onClick,
     },
     children: [title],
+  };
+}
+
+Button.propTypes = {
+  title: "string",
+  onClick: "function",
+  style: "object",
+};
+
+export default function Button(props) {
+  for (let key in Button.propTypes) {
+    if (typeof props[key] !== Button.propTypes[key]) {
+      console.log(
+        `Invalid prop ${key} for Button component. Expected ${
+          Button.propTypes[key]
+        }, received ${typeof props[key]}`
+      );
+    }
+  }
+  const baseStyle = {
+    backgroundColor: "grey",
+    borderRadius: "5px",
+  };
+  return {
+    type: "button",
+    attributes: {
+      style: { ...baseStyle, ...props.style },
+    },
+    events: {
+      click: props.onClick,
+    },
+    children: [props.title],
   };
 }
